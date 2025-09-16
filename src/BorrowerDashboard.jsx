ca75654ca993d6f553550b9a77eedad668bb5e38
Isaac <alfaro.isaac10@gmail.com>
2025-09-16 14:15:16 -0400

    fix: Pass oportunidad object directly to SavingsCalculator and DocumentManager

--- src/BorrowerDashboard.jsx
+++ src/BorrowerDashboard.jsx
@@ -284,7 +284,7 @@ const BorrowerDashboard = () => {
       {solicitud.estado === 'pre-aprobado' && (
         <>
           {console.log('Solicitud en BorrowerDashboard:', solicitud)} {/* LOGGING AQUI */}
-          <SavingsCalculator solicitud={solicitud} oportunidad={solicitud.oportunidades[0]} />
+          <SavingsCalculator solicitud={solicitud} oportunidad={solicitud.oportunidades} />
           <DocumentManager 
             solicitud={solicitud} 
             user={user}
