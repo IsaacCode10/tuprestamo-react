3859a53d0ad607d0bae26feb958d5cb13771b177
Isaac <alfaro.isaac10@gmail.com>
2025-10-11 19:04:36 -0400

    feat(landing): Align marketing text with business model rates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,13 +17,13 @@ const Hero = () => {
           <div className="cta-item">
             <Link to="prestatarios" smooth={true} duration={500} className="btn btn--secondary">Quiero refinanciar</Link>
             <div className="cta-box">
-              <p>REDUCE TUS INTERESES DE TARJETA DE CRÉDITO HASTA 17% ANUAL</p>
+              <p>REDUCE TUS INTERESES DE TARJETA DE CRÉDITO CON TASAS DESDE 15% ANUAL</p>
             </div>
           </div>
           <div className="cta-item">
             <Link to="inversionistas" smooth={true} duration={500} className="btn btn--primary">Quiero invertir</Link>
             <div className="cta-box">
-              <p>OBTÉN RENDIMIENTOS DESDE 12% ANUAL FINANCIANDO PRÉSTAMOS</p>
+              <p>OBTÉN RENDIMIENTOS HASTA 15% ANUAL FINANCIANDO PRÉSTAMOS</p>
             </div>
           </div>
         </div>
